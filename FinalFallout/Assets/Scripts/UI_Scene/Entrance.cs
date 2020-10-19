ae1c6d0afdde3754711ed93d858cb217b299d838
RoanOak127 <54964349+RoanOak127@users.noreply.github.com>
2020-10-19 14:43:20 -0500

    Organized Script Folder
    Organized the Script Folder
    This is mostly to try to clear out my github trying to repush asset data

---
+++ FinalFallout/Assets/Scripts/UI_Scene/Entrance.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Entrance : MonoBehaviour
+{
+    public string entrancePW;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PlayerMovement.instance.levelPW == entrancePW)
+        {
+            //put the player in the new scene
+            PlayerMovement.instance.transform.position = transform.position; 
+        }
+        else
+        {
+            Debug.Log("Nothing");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Tab)){
+            openMenu();
+        }
+        
+    }
+
+    void openMenu(){
+        PlayerPrefs.SetInt("PreviousScene", SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene (sceneName:"MainMenu");
+    }
+}
---
+++ FinalFallout/Assets/Scripts/UI_Scene/SceneFade.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneFade : MonoBehaviour
+{
+    public Image blackImage;
+    [SerializeField] private float alpha;
+
+    private void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
+    public void FadeTo(string sceneName)
+    {
+        StartCoroutine(FadeOut(sceneName));
+    }
+    IEnumerator FadeIn()
+    {
+        alpha = 1;
+        while (alpha > 0)
+        {
+            alpha -= Time.deltaTime;
+            blackImage.color = new Color(0, 0, 0, alpha);
+            yield return new WaitForSeconds(0);
+        }
+    }
+    IEnumerator FadeOut(string levelName)
+    {
+        alpha = 0;
+        while (alpha <1)
+        {
+            alpha += Time.deltaTime;
+            blackImage.color = new Color(0, 0, 0, alpha);
+            yield return null;
+        }
+        SceneManager.LoadScene(levelName);
+    }
+}
