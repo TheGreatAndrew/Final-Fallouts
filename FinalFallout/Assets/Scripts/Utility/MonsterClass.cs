3e983528ec04d6bcb1bda7702fc35d23844303ca
RoanOak127 <54964349+RoanOak127@users.noreply.github.com>
2020-11-25 14:40:05 -0600

    Added Enemy Prefabs and Monster Class
    Monster prefabs added with base stats and images. Also added a Monster Class

---
+++ FinalFallout/Assets/Scripts/Utility/MonsterClass.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MonsterClass : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer displayMonster; //image attached to gameobject
+
+    public Sprite monsterSprite;
+    public int health;
+    public int attack;
+    public int defense;
+    public int rewards;
+    public int numArms;
+    public float chanceAttack2;
+
+
+
+}
