86502fd8b7b7f4986dd3f1221b9189855d115474
coreyblanke <car4corey@gmail.com>
2020-11-24 21:28:26 -0600

    Added monster enum utilty class
    look up how to use enums in unity and see my comment block in monsterenum.cs

---
+++ FinalFallout/Assets/Scripts/Utility/MonsterEnum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterEnum : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    /*
+    * This script contains information for the monstertypes, please use the monstertypes enum when you are calling the
+     * enemy generator or doing anything in regards to instantiating monsters
+     *
+     * feel free to add members as you see fit, but when you add members in, please have a sprite associated with it
+     * otherwise i will just use a placeholder
+    */
+}
+
+public enum MonsterType
+{
+    Slime,
+    Mushroom,
+    Gecko,
+    Skeleton,
+    Jelly,
+    Boss
+}
